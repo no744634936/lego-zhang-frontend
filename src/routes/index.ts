59e3c018b7c2be3bd75c272f9847d0ae3157e8ec
zhanghaifeng2019 <zhang@rentracks.co.jp>
2021-07-03 10:22:20 +0900

    good

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
+
 import Home from '../views/Home.vue';
 import Editor from '../views/Editor.vue';
 import TemplateDetail from '../views/TemplateDetail.vue';
@@ -7,7 +8,7 @@ const router = createRouter({
     // history: createWebHistory(), 必须要写上，这句话的背后原理还不太懂
     history: createWebHistory(),
     routes: [
-        // meta是这个路由带着的额外的信息、App.vue 文件里有用来明确是否使用header
+        // meta是这个路由带着的额外的信息、App.vue 文件里用来明确是否使用header
         { path: '/', name: 'home', component: Home,meta:{needHeader:true}},
         { path: '/editor', name: 'editor', component: Editor },
         { path: '/template/:id', name: 'templateDetail', component: TemplateDetail,meta:{needHeader:true} },
