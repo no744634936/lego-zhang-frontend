5718b1a672ddcb4720ba9dfd14c69bbbacd3e9e5
zhanghaifeng2019 <zhang@rentracks.co.jp>
2021-07-06 08:38:01 +0900

    good

--- src/components/ComponentsList.vue
+++ src/components/ComponentsList.vue
@@ -1,6 +1,9 @@
 <template>
     <!-- 
         为什么要用div来隔离一下呢？因为需要在LText这个组件上添加click事件，但是并不想影响到组件本身的click事件？
+        LText 组件上本身已经有了一个click事件了，点击一下跳转到另一个网页的事件。
+        现在又想点击一下组件，让他从左边的模板列表跑到中间的编辑器，也需要一个click事件，
+        为了区分开两个click事件，所以用div来隔离一下，给div一个新的click事件
     <div class="component-list">
         <LText v-for="(item,index) in list" :key="index" v-bind="item" class="component-item"></LText>
     </div> 
@@ -10,7 +13,6 @@
              <!-- v-bind="item"  的意思就是将 item 这个对象传给LText里的props 这是v-bind单独使用时的作用-->
             <LText v-bind="item"></LText>
         </div>
-        
     </div> 
 </template>
 
@@ -18,8 +20,8 @@
 import { defineComponent } from 'vue'
 import LText from './LText.vue'
 export default defineComponent({
-    name:'components-list',
-    emits:['on-item-click'],//发送一个事件
+    name:'componentsList',
+    emits:['onItemClick'],//发送一个事件给父组件
     props:{
         list:{type:Array,required:true}
     },
@@ -28,7 +30,7 @@ export default defineComponent({
     },
     setup(props,context){
         const onItemClick=(data: any)=>{
-            context.emit('on-item-click',data)
+            context.emit('onItemClick',data)
         }
 
         return {
