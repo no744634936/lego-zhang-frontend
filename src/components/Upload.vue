c7a3ecf86d6b0cb2072c113c28f5ee5557695c21
zhanghaifeng2019 <zhang@rentracks.co.jp>
2021-07-19 22:27:07 +0900

    good

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -1,15 +1,24 @@
 <template>
     <div class='file-upload'>
         <!-- input元素是被隐藏的，点击button然后触发input的click事件，就可以传文件了 -->
-        <button @click="triggerUpload" :disabled="isUploading">
-            <span v-if="isUploading">正在上传</span>
-            <span v-else>点击上传</span>
-        </button>
+        <div class="upload-area" @click="triggerUpload" >
+            <slot  v-if="isUploading" name="loading">
+                <button disabled>正在上传</button>
+            </slot>
+            <slot name="uploaded" v-else-if="lastFileData && lastFileData.loaded" :uploadedData="lastFileData.response">
+                <button>点击上传</button>
+            </slot>
+            <slot  v-else name="default">
+                <button>点击上传</button>
+            </slot>
+        </div>
         <input ref="fileInput" type="file" :style="{display: 'none'}" @change="handleFileUpload">
         <ul>
             <li :class="`upload-file upload-${file.status}`" v-for="file in uploadedFiles" :key='file.uid'>
+                <span v-if="file.status === 'loading'" class="file-icon"><LoadingOutlined/></span>
+                <span v-else class="file-icon"><FileOutlined/></span>
                 <span class="filename">{{file.name}}</span>
-                <button class="delete-icon" @click="removeFile(file.uid)">delete</button>
+                <span class="delete-icon" @click="removeFile(file.uid)"><DeleteOutlined/></span>
             </li>
         </ul>
     </div>
@@ -19,6 +28,9 @@
 import { defineComponent, ref, reactive, computed } from 'vue'
 import axios from 'axios'
 import { uuid  } from 'uuidv4'
+import { last } from 'lodash-es'
+import { DeleteOutlined, LoadingOutlined, FileOutlined } from '@ant-design/icons-vue'
+
 type UploadSatuts = 'ready' | 'loading' | 'success' | 'error'
 
 export interface UploadFile{
@@ -27,6 +39,7 @@ export interface UploadFile{
     name: string;
     status: UploadSatuts;
     raw: File;
+    response?: any;
 }
 
 export default defineComponent({
@@ -36,6 +49,11 @@ export default defineComponent({
         type: String,
         required: true,
     }
+  },
+  components: {
+    DeleteOutlined,
+    LoadingOutlined,
+    FileOutlined
   },
   setup(props){
     // 要拿到input这个html元素，需要使用ref
@@ -84,12 +102,15 @@ export default defineComponent({
             result = await axios.post( props.api_url,formData,{
                 headers:{
                     'content-Type':'multipart/form-data',
-                    'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IjEzNjM4MTM3MDA0IiwicGhvbmVOdW1iZXIiOiIxMzYzODEzNzAwNCIsIm5pY2tOYW1lIjoia2tra2siLCJpYXQiOjE2MjY2MTQwNjUsImV4cCI6MTYyNjcwMDQ2NX0.PXHEL4UjEQzlVEDlXmhWnYLo3EJ05UBHM6CmjgxT50s',                }
+                    'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IjEzNjM4MTM3MDA0IiwicGhvbmVOdW1iZXIiOiIxMzYzODEzNzAwNCIsIm5pY2tOYW1lIjoia2tra2siLCJpYXQiOjE2MjY3MDA5NjYsImV4cCI6MTYyNjc4NzM2Nn0.rPzDg2PADSqaRN7zz37bFJtx1qOHQjmiAw-4DkfjxAY',                
+                }
             })
 
             if(result.data.errno == 0){
                 console.log('上传图片成功');
                 fileObj.status='success'
+                fileObj.response=result.data.data
+
             }else{
                 console.log('上传图片出错');
                 fileObj.status='error'
@@ -106,6 +127,19 @@ export default defineComponent({
         console.log('testtest',result);
     }
 
+    const lastFileData=computed(()=>{
+        const lastFile=last(uploadedFiles.value)
+        if(lastFile){
+            return {
+                loaded: lastFile.status==='success',
+                data: lastFile.response
+            }
+        }else{
+            return false
+        }
+        
+    })
+
     const removeFile=(id: string)=>{
         uploadedFiles.value=uploadedFiles.value.filter(file=>file.uid!==id)
     }
@@ -117,20 +151,79 @@ export default defineComponent({
         isUploading,
         uploadedFiles,
         removeFile,
-        
+        lastFileData,
     }
   }
 })
 </script>
 
-<style scoped>
-.upload-loading{
-    color: yellow;
+<style lang="scss">
+.upload-list {
+  margin: 0;
+  padding: 0;
+  list-style-type: none;  
 }
-.upload-success{
-    color: green;
-}
-.upload-error{
-    color: red;
+.upload-list li {
+  transition: all .5s cubic-bezier(.55,0,.1,1);
+  font-size: 14px;
+  line-height: 1.8;
+  margin-top: 5px;
+  box-sizing: border-box;
+  border-radius: 4px;
+  min-width: 200px;
+  position: relative;
+  &:first-child {
+    margin-top: 10px;
+  }
+  .upload-list-thumbnail {
+    vertical-align: middle;
+    display: inline-block;
+    width: 70px;
+    height: 70px;
+    position: relative;
+    z-index: 1;
+    background-color: #fff;
+    object-fit: cover;
+  }
+  .file-icon {
+    svg {
+      margin-right: 5px;
+      color: rgba(0, 0, 0, 0.45);
+    }
+  }
+  .filename {
+    margin-left: 5px;
+    margin-right: 40px;
+  }
+  &.upload-error {
+    color: #f5222d;
+    svg {
+      color: #f5222d;
+    }
+  }
+  .file-status {
+    display: block;
+    position: absolute;
+    right: 5px;
+    top: 0;
+    line-height: inherit;
+  }
+  .delete-icon {
+    display: none;
+    position: absolute;
+    right: 7px;
+    top: 0;
+    line-height: inherit;
+    cursor: pointer;
+  }
+  &:hover {
+    background-color: #efefef;
+    .file-status {
+      display: none;
+    }
+    .delete-icon {
+      display: block;
+    }
+  }
 }
 </style>
