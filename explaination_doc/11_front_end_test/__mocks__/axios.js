d7286f33e85a6b0f6d44d4fb6d3a53f27fc91309
zhanghaifeng2019 <zhang@rentracks.co.jp>
2021-12-21 17:30:42 +0900

    rrewrite doc

--- explaination_doc/11_front_end_test/__mocks__/axios.js
+++
@@ -1,8 +0,0 @@
-const axios= {
-    // 意思就是相当与调用axios的get方法的时候，返回使用的是jest.fn(()=>{return Promise.resolve({data:{name:'zhanghaifeng1123'}})})
-    get: jest.fn(()=>{
-        return Promise.resolve({data:{name:'zhanghaifeng1123'}})
-    })
-}
-
-module.exports=axios
